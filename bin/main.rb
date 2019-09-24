262107b694f8921632b31dc46a26bd80137b332e
noomdalv <vladlunasan@gmail.com>
2019-09-24 17:32:01 -0500

    added welcome message and greeting player method

---
+++ bin/main.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+class Main
+
+welcome = "Welcome to Tic Tac Toe"
+p welcome
+
+puts "Player 1 please enter your name: "
+p1 = gets.chomp
+greeting(p1)
+
+puts "Player 2 please enter your name: "
+p2 = gets.chomp
+greeting(p2)
+
+def greeting(name)
+	puts "Hello #{name}"
+end
+
+
+end
