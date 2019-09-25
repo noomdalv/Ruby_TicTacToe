a7b74910a1d94f693eef1b8b86b389554ff8bc09
noomdalv <vladlunasan@gmail.com>
2019-09-25 12:39:17 -0500

    added welcome and user prompts

--- bin/main.rb
+++ bin/main.rb
@@ -3,19 +3,31 @@
 
 # main class.
 class Main
+	welcome = p "Welcome to Ruby Tic Tac Toe"
+
+	p "Player 1 please enter your name: "
+	p1name = gets.chomp
+	p "Hello #{p1name}, you will play with X"
+
+	p "Player 2 please enter your name: "
+	p2name = gets.chomp
+	p "Hello #{p2name}, you will play with O"
 end
 
 # Game Board
 class Board
   def initialize
-    @board = Array.new(9) { ' ' }
+    @board = Array.new(9) { '?' }
   end
 
   def display
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts ' ----------- '
+    puts ' ---------- '
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts ' ----------- '
+    puts ' ---------- '
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 end
+
+grid = Board.new
+grid.display
