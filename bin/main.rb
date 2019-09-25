2c2a1359d1a73df45023c1e1e42a21f55996e823
noomdalv <vladlunasan@gmail.com>
2019-09-25 13:03:44 -0500

    added user ending prompt and choose turn inputs

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,23 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
+# Game Board
+class Board
+  def initialize
+    @board = Array.new(9) { '-' }
+  end
+
+  def display
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts ' ---------- '
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts ' ---------- '
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def insertMove(position, token = "X")
+	  @board[position] = token
+  end
+end
 
 # main class.
 class Main
@@ -12,22 +30,22 @@ class Main
 	p "Player 2 please enter your name: "
 	p2name = gets.chomp
 	p "Hello #{p2name}, you will play with O"
+
+	grid = Board.new
+	grid.display
+
+	p "Player 1 Turn, please select your next move: "
+	p1turn = gets.chomp.to_i
+	grid.insertMove(p1turn)
+
+	grid.display
+
+	p "Player 2 Turn, please select your next move: "
+	p2turn = gets.chomp.to_i
+	grid.insertMove(p2turn, "O")
+
+	grid.display
+
+ending = p "Thank you for playing Tic Tac Toe"
+ending
 end
-
-# Game Board
-class Board
-  def initialize
-    @board = Array.new(9) { '?' }
-  end
-
-  def display
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts ' ---------- '
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts ' ---------- '
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-  end
-end
-
-grid = Board.new
-grid.display
