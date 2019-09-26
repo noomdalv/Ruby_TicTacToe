e61fe2dfc67d2f7348ac1165a126616184fb74b9
noomdalv <vladlunasan@gmail.com>
2019-09-26 16:28:55 -0500

    added winning combinations and won method for game_manager

--- bin/main.rb
+++ bin/main.rb
@@ -4,19 +4,18 @@
 require_relative '../lib/game_manager.rb'
 
 class Main
-
-	def initialize
-     @game = GameManager.new
-     @board = @game.init_board(9)
+  def initialize
+    @game = GameManager.new
+    @board = @game.init_board(9)
   end
 
-   def display
-     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-     puts ' ---------- '
-     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-     puts ' ---------- '
-     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-   end
+  def display
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts ' ---------- '
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts ' ---------- '
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
 
   def turn
     puts 'Choose a spot between 1-9'
@@ -38,8 +37,7 @@ class Main
       puts "It's a draw!"
     end
   end
-
 end
 
-  main = Main.new
-  main.play
+main = Main.new
+main.play
--- lib/game_manager.rb
+++ lib/game_manager.rb
@@ -6,15 +6,23 @@ class GameManager
   end
 
   WIN_COMBINATIONS = [
-   [0, 1, 2],
-   [3, 4, 5],
-   [6, 7, 8],
-   [0, 3, 6],
-   [1, 4, 7],
-   [2, 5, 8],
-   [6, 4, 2],
-   [0, 4, 8]
- ].freeze
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [6, 4, 2],
+    [0, 4, 8]
+  ].freeze
+
+  def won?
+    WIN_COMBINATIONS.detect do |combo|
+      @board[combo[0]] == @board[combo[1]] &&
+        @board[combo[1]] == @board[combo[2]] &&
+        space_filled?(combo[0])
+    end
+  end
 
   def init_board(grid)
     @board = Array.new(grid) { '-' }
