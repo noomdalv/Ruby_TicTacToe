bd4728a7c7e62590a184276b5e6e95e664460676
noomdalv <vladlunasan@gmail.com>
2019-09-26 16:55:39 -0500

    rubocop check

--- bin/main.rb
+++ bin/main.rb
@@ -4,21 +4,20 @@
 require_relative '../lib/game_manager.rb'
 
 class Main
-
   def initialize
     @game = GameManager.new
     @board = @game.init_board(9)
   end
 
   def welcome
-    #Welcome Message
-    puts "Welcome to Tic Tac Toe"
+    # Welcome Message
+    puts 'Welcome to Tic Tac Toe'
 
-    #Request for player names and save them to p1 and p2
-    puts "Player 1 please enter your name: "
+    # Request for player names and save them to p1 and p2
+    puts 'Player 1 please enter your name: '
     p1 = gets.chomp
 
-    puts "Player 2 please enter your name: "
+    puts 'Player 2 please enter your name: '
     p2 = gets.chomp
 
     puts "Player 1: #{p1}, Token: X"
@@ -28,7 +27,7 @@ class Main
   end
 
   def ending
-    puts "Thank you for playing Tic Tac Toe."
+    puts 'Thank you for playing Tic Tac Toe.'
   end
 
   def display
@@ -39,8 +38,8 @@ class Main
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
-  #Game begins, player 1 starts choosing between 1-9
-  #if the input is different valid_move returns false and input is request again
+  # Game begins, player 1 starts choosing between 1-9
+  # if the input is different valid_move returns false and input is request again
   def turn
     current = @game.current_player
     puts "Player #{current}, choose a spot between 1-9"
@@ -49,15 +48,15 @@ class Main
     if @game.valid_move?(spot)
       @game.set_input(spot, @game.current_player)
     else
-      puts "Please enter a valid option between 1-9"
+      puts 'Please enter a valid option between 1-9'
       display
       turn
     end
     display
   end
 
-  #Turn method is called for players to fill the board until @game.over? method is true
-  #If total moves = 9 and no combinations detected, its a draw, if a combination is detected, @game.won is executed
+  # Turn method is called for players to fill the board until @game.over? method is true
+  # If total moves = 9 and no combinations detected, its a draw, if a combination is detected, @game.won is executed
   def play
     turn until @game.over?
     if @game.won?
