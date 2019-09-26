5255b89a33382e08e80b9c33a277814442b5225d
noomdalv <vladlunasan@gmail.com>
2019-09-26 16:21:24 -0500

    added win_combinatios for game_manager.rb

--- lib/game_manager.rb
+++ lib/game_manager.rb
@@ -5,6 +5,17 @@ class GameManager
     @board = nil
   end
 
+  WIN_COMBINATIONS = [
+   [0, 1, 2],
+   [3, 4, 5],
+   [6, 7, 8],
+   [0, 3, 6],
+   [1, 4, 7],
+   [2, 5, 8],
+   [6, 4, 2],
+   [0, 4, 8]
+ ].freeze
+
   def init_board(grid)
     @board = Array.new(grid) { '-' }
   end
