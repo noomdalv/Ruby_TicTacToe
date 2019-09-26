d3ea9c7d5bb5dbfb9628e81b1b51b8b294bc2088
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-09-26 15:07:39 -0600

    add space_filled?, board_index and valid_move? methods

--- lib/game_manager.rb
+++ lib/game_manager.rb
@@ -12,4 +12,16 @@ class GameManager
   def set_input(index, input = 'X')
     @board[index] = input
   end
+
+  def space_filled?(index)
+    @board[index] == 'X' || @board[index] == 'O'
+  end
+
+  def board_index(input_index)
+    input_index.to_i - 1
+  end
+
+  def valid_move?(arr_pos)
+    arr_pos.between?(0, 8) && !space_filled?(arr_pos)
+  end
 end
