b2aca47b7b4489092ba836584298df4e54cf29ba
noomdalv <vladlunasan@gmail.com>
2019-09-26 16:15:24 -0500

    added initialize and display methods for Main class

--- bin/main.rb
+++ bin/main.rb
@@ -1,79 +1,24 @@
-#!/usr/bin/env ruby
-# frozen_string_literal: true
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require_relative '../lib/game_manager.rb'
+
+class Main
+
+	def initialize
+     @game = GameManager.new
+     @board = @game.init_board(9)
+  end
+
+   def display
+     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+     puts ' ---------- '
+     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+     puts ' ---------- '
+     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+   end
 
-require_relative '../lib/game_manager.rb'
-
-=begin
-# Game Board
-class Board
-  def initialize
-    @board = Array.new(9) { '-' }
-  end
-
-  def display
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts ' ---------- '
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts ' ---------- '
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-  end
-
-  def insertMove(position, token = 'X')
-    @board[position] = token
-  end
-end
-=end
-
-# main class.
-class Main
-  def initialize
-    @game = GameManager.new(game)
-    @board = Array.new(9) { '-' }
-  end
-
-
-  def show_input
-    @game.set_input(0)
-  end
-
-  def display
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts ' ---------- '
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts ' ---------- '
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-  end
-
-
-  # welcome = 'Welcome to Ruby Tic Tac Toe'
-  # p welcome
-
-  # p 'Player 1 please enter your name: '
-  # p1name = gets.chomp
-  # p "Hello #{p1name}, you will play with X"
-
-  # p 'Player 2 please enter your name: '
-  # p2name = gets.chomp
-  # p "Hello #{p2name}, you will play with O"
-
-  # grid = Board.new
-  # grid.display
-
-  # p 'Player 1 Turn, please select your next move: '
-  # p1turn = gets.chomp.to_i
-  # grid.insertMove(p1turn)
-
-  # grid.display
-
-  # p 'Player 2 Turn, please select your next move: '
-  # p2turn = gets.chomp.to_i
-  # grid.insertMove(p2turn, 'O')
-
-  # grid.display
-
-  # ending = p 'Thank you for playing Tic Tac Toe'
-  # ending
-end
-
-main = Main.new
-main.show_input
+  end
+
+  main = Main.new
+  main.play
