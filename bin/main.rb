3ab000f0d6d495ec3d585954fa8d614209367657
noomdalv <vladlunasan@gmail.com>
2019-09-27 17:12:22 -0500

    added player class implementation

--- bin/main.rb
+++ bin/main.rb
@@ -3,27 +3,30 @@
 # frozen_string_literal: true
 
 require_relative '../lib/game_manager.rb'
+require_relative '../lib/player.rb'
 
 class Main
   def initialize
     @game = GameManager.new
 
     @board = @game.init_board(9)
-    @player1 = nil
-    @player2 = nil
+    @player1 = Player.new
+    @player2 = Player.new
   end
 
   def welcome
     puts "\nWelcome to Tic Tac Toe\n\n"
 
     print 'Player 1 please enter your name: '
-    @player1 = gets.chomp
+    @player1.name = gets.chomp
+    @player1.symbol = 'X'
 
     print "\nPlayer 2 please enter your name: "
-    @player2 = gets.chomp
+    @player2.name = gets.chomp
+    @player2.symbol = 'O'
 
-    puts "\nPlayer 1: #{@player1}, Token: X"
-    puts "Player 2: #{@player2}, Token: O\n"
+    puts "\nPlayer 1: #{@player1.name}, Symbol: #{@player1.symbol}"
+    puts "Player 2: #{@player2.name}, Symbol: #{@player2.symbol}\n"
 
     display
   end
@@ -44,31 +47,35 @@ class Main
     puts "       #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
-  def turn
+  def choose_position
     current = @game.current_player
     if current == 'X'
-      print "\n #{@player1}, choose a spot between 1-9: "
+      print "\n #{@player1.name}, choose a position between 1-9: "
     else
-      print "\n #{@player2}, choose a spot between 1-9: "
+      print "\n #{@player2.name}, choose a position between 1-9: "
     end
+  end
+
+  def turn
+    choose_position
     spot = gets.strip
     spot = @game.board_index(spot)
     if @game.valid_move?(spot)
       @game.set_input(spot, @game.current_player)
+      display
     else
-      print "\nPlease enter a valid option between 1-9: \n"      
+      print "\nPlease enter a valid option between 1-9: \n"
       turn
     end
-    display
   end
 
   def play
     turn until @game.over?
     if @game.won?
       if @board[@game.won?.first] == 'X'
-        puts "\nCongratulations #{@player1}!"
+        puts "\nCONGRATULATIONS #{@player1.name}!"
       else
-        puts "\nCongratulations #{@player2}!"
+        puts "\nCONGRATULATIONS #{@player2.name}!"
       end
     elsif @game.draw?
       puts "\nIt's a draw!"
--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 class Player
   attr_accessor :name, :symbol
 
-  def initialize(name, symbol)
+  def initialize(name = nil, symbol = nil)
     @name = name
     @symbol = symbol
-  end  
-
+  end
 end
