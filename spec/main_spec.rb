6b6b270a14dd60b3433924b77d66accb9b4e8923
noomdalv <vladlunasan@gmail.com>
2019-09-30 10:42:06 -0500

    added rspec files

---
+++ spec/main_spec.rb
@@ -0,0 +1,28 @@
+require '../bin/main.rb'
+
+RSpec.describe Main do
+	describe "#welcome" do
+		it "prints player names with their symbols" do
+			expect().to eql()
+		end
+	end
+end
+
+RSpec.describe Main do
+	describe "#" do
+		it "" do
+			expect().to eql()
+		end
+	end
+end
+
+
+
+#RSpec.describe Calculator do
+#  describe "#add" do
+#    it "returns the sum of two numbers" do
+#      calculator = Calculator.new
+#      expect(calculator.add(5, 2)).to eql(7)
+#    end
+#  end
+#end
